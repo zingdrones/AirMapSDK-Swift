f0f59cad445cf9ccb2b87379f136a7e02ee49c4e
Adolfo Martinelli <adolfo@airmap.com>
2016-08-16 09:52:28 -0700

    Add empty macOS example target

---
+++ Example/macOS/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  AirMapSDK-Example-macOS
+//
+//  Created by Adolfo Martinelli on 8/16/16.
+//  Copyright © 2016 AirMap, Inc. All rights reserved.
+//
+
+import Cocoa
+import AirMap
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
