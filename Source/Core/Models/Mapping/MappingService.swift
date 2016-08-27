f6490e48f45d837e21af87357674a98997e69513
Rocky Demoff <rocky@airmap.com>
2016-08-27 10:03:12 -0700

    Moved TFRs outside of Special Use for AirMapLayerType

--- Source/Core/Models/Mapping/MappingService.swift
+++ Source/Core/Models/Mapping/MappingService.swift
@@ -245,8 +245,10 @@ public enum AirMapLayerType: Int, CustomStringConvertible {
 		case .Schools:
 			return .School
 
-		case .TFRs,
-		     .Restricted,
+		case .TFRs:
+			return .TFR
+
+		case .Restricted,
 		     .Prohibited:
 			return .SpecialUse
 
@@ -395,19 +397,19 @@ public enum AirMapMapTheme: Int {
 internal class MappingService {
 
 	/**
-	
+
 	Generates and returns map tile source url based upon Map Layers & Theme.
 
 	- parameter layers: An array of AirMapMapLayer's.
 	- parameter theme: An AirMapMapTheme.
 
 	- returns: NSURL?
-	
+
 	*/
 	func tileSourceUrl(layers: [AirMapLayerType], theme: AirMapMapTheme) -> NSURL? {
 
 		let apiKey = AirMap.configuration.airMapApiKey
-		let tiles  = layers.count == 0 ? "_-_" : layers.flatMap{$0.type}.joinWithSeparator(",")
+		let tiles  = layers.count == 0 ? "_-_" : layers.flatMap {$0.type}.joinWithSeparator(",")
 		let urlString = Config.AirMapApi.mapTilesUrl + "/\(tiles)?&theme=\(theme.name)&apikey=\(apiKey)&token=\(apiKey)"
 
 		return  NSURL(string: urlString)
