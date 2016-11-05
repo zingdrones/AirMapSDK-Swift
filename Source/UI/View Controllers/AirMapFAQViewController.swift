f2d3a453ee1794fa03987e7c2a6c42784e88d141
Adolfo Martinelli <adolfo@airmap.com>
2016-11-04 18:05:53 -0700

    Remove FAQs header from html and add to iOS view controller

--- Source/UI/View Controllers/AirMapFAQViewController.swift
+++ Source/UI/View Controllers/AirMapFAQViewController.swift
@@ -24,10 +24,24 @@ class AirMapFAQViewController: UIViewController {
 		let url = "https://cdn.airmap.io/static/webviews/faq.html#\(section?.rawValue ?? "")"
 		let request = NSURLRequest(URL: NSURL(string: url)!)
 		webView.loadRequest(request)
+		webView.delegate = self
 	}
 	
 	@IBAction func dismiss() {
 		dismissViewControllerAnimated(true, completion: nil)
 	}
 	
+}
+
+extension AirMapFAQViewController: UIWebViewDelegate {
+	
+	func webViewDidFinishLoad(webView: UIWebView) {
+		webView.stringByEvaluatingJavaScriptFromString(
+			"var h3Tags = document.getElementsByTagName('h3');" +
+			"for (var i = h3Tags.length; i--;) {" +
+			"  var h3 = h3Tags[i];" +
+			"  h3.parentNode.removeChild(h3);" +
+			"}"
+		)
+	}
 }
