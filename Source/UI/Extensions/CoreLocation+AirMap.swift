c07ba798e893ae0c1abded2efef465588364e611
Adolfo Martinelli <adolfo@airmap.com>
2017-03-30 12:57:08 -0700

    Update deprecated Pi values

--- Source/UI/Extensions/CoreLocation+AirMap.swift
+++ Source/UI/Extensions/CoreLocation+AirMap.swift
@@ -15,17 +15,17 @@ extension CLLocationCoordinate2D {
 		let degreesBetweenPoints = 4.0
 		let numberOfPoints = floor(360.0 / degreesBetweenPoints)
 		let distRadians: Double = withMeterRadius / 6371000.0
-		let centerLatRadians: Double = coordinate.latitude * M_PI / 180
-		let centerLonRadians: Double = coordinate.longitude * M_PI / 180
+		let centerLatRadians: Double = coordinate.latitude * .pi / 180
+		let centerLonRadians: Double = coordinate.longitude * .pi / 180
 		var coordinates = [CLLocationCoordinate2D]()
 		
 		for index in 0..<Int(numberOfPoints) {
 			let degrees: Double = Double(index) * Double(degreesBetweenPoints)
-			let degreeRadians: Double = degrees * M_PI / 180
+			let degreeRadians: Double = degrees * .pi / 180
 			let pointLatRadians: Double = asin(sin(centerLatRadians) * cos(distRadians) + cos(centerLatRadians) * sin(distRadians) * cos(degreeRadians))
 			let pointLonRadians: Double = centerLonRadians + atan2(sin(degreeRadians) * sin(distRadians) * cos(centerLatRadians), cos(distRadians) - sin(centerLatRadians) * sin(pointLatRadians))
-			let pointLat: Double = pointLatRadians * 180 / M_PI
-			let pointLon: Double = pointLonRadians * 180 / M_PI
+			let pointLat: Double = pointLatRadians * 180 / .pi
+			let pointLon: Double = pointLonRadians * 180 / .pi
 			let point: CLLocationCoordinate2D = CLLocationCoordinate2DMake(pointLat, pointLon)
 			coordinates.append(point)
 		}
