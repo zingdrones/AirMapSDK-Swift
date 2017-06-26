5c1ce42ccc100ed1ea74412551aba7af7a00a82d
Adolfo Martinelli <adolfo@airmap.com>
2017-06-26 12:43:03 -0700

    Update advisory colors

--- Source/UI/Extensions/Color+AirMap.swift
+++ Source/UI/Extensions/Color+AirMap.swift
@@ -34,11 +34,11 @@ public extension Color {
 	}
 	
 	public static var airMapYellow: Color {
-		return Color(red: 249.0/255.0, green: 229.0/255.0, blue: 71.0/255.0, alpha: 1.0)
+		return Color(red: 249.0/255.0, green: 235.0/255.0, blue: 59.0/255.0, alpha: 1.0)
 	}
 	
 	public static var airMapOrange: Color {
-		return Color(red: 252.0/255.0, green: 76.0/255.0, blue: 3.0/255.0, alpha: 1.0)
+		return Color(red: 252.0/255.0, green: 140.0/255.0, blue: 0.0/255.0, alpha: 1.0)
 	}
 	
 	public static var airMapLightGray: Color {
