0e8346a572594b148f73b827efedd843af7dd24a
Adolfo Martinelli <adolfo@airmap.com>
2017-08-02 10:18:03 -0700

    Cleanup flight features and add documentation

--- Source/Core/Models/Rules/AirMapFlightFeature.swift
+++ Source/Core/Models/Rules/AirMapFlightFeature.swift
@@ -6,29 +6,39 @@
 //  Copyright © 2017 AirMap, Inc. All rights reserved.
 //
 
-import Foundation
-import ObjectMapper
-
-public class AirMapFlightFeature: Mappable {
+public struct AirMapFlightFeature {
 	
+	/// The unique identifier for the flight feature
 	public let code: String
+	
+	/// A textual description of the flight feature
 	public let description: String
+	
+	/// The type of input the flight feature requires
 	public let inputType: InputType
+	
+	/// The measurement type of the input
 	public let measurementType: MeasurementType
+	
+	/// The unit to use for the measurement type
 	public let measurementUnit: MeasurementUnit?
 
+	/// The flight feature's concrete Type
 	public enum InputType: String {
 		case bool
 		case float
+		case string
 
 		var type: Any.Type {
 			switch self {
 			case .bool:  return Bool.self
 			case .float: return Double.self
+			case .string: return String.self
 			}
 		}
 	}
 	
+	/// An enumeration of possible measurement types
 	public enum MeasurementType: String {
 		case speed
 		case weight
@@ -36,27 +46,24 @@ public class AirMapFlightFeature: Mappable {
 		case binary
 	}
 	
+	/// An enumeration of possible measurement units
 	public enum MeasurementUnit: String {
 		case meters
 		case kilograms
 		case boolean
 		case metersPerSecond = "meters_per_sec"
 	}
+}
+
+import ObjectMapper
+
+extension AirMapFlightFeature: ImmutableMappable {
 	
-	public required init?(map: Map) {
-		do {
-			code            =  try  map.value("flight_feature")
-			description     =  try  map.value("description")
-			inputType       =  try  map.value("input_type")
-			measurementType = (try? map.value("measurement_type")) ?? .binary
-			measurementUnit =  try? map.value("measurement_unit")
-		}
-		catch let error {
-			print(error)
-			return nil
-		}
+	public init(map: Map) throws {
+		code            =  try  map.value("flight_feature")
+		description     =  try  map.value("description")
+		inputType       =  try  map.value("input_type")
+		measurementType = (try? map.value("measurement_type")) ?? .binary
+		measurementUnit =  try? map.value("measurement_unit")
 	}
-	
-	public func mapping(map: Map) {}
-	
 }
