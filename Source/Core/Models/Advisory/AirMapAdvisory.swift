53240fe46838355286a032df8b3b4246d025aa8e
Adolfo Martinelli <adolfo@airmap.com>
2017-08-04 16:43:32 -0700

    Cleanup and document AirMapAdvisory

--- Source/Core/Models/Advisory/AirMapAdvisory.swift
+++ Source/Core/Models/Advisory/AirMapAdvisory.swift
@@ -6,34 +6,81 @@
 //  Copyright © 2017 AirMap, Inc. All rights reserved.
 //
 
-import Foundation
+/// An airspace object that interects an intended area of operation.
+public struct AirMapAdvisory {
+	
+	/// The advisory's unique identifier
+	public let id: String
+	
+	/// The airspace classification category
+	public let type: AirMapAirspaceType
+	
+	/// A color representative of the advisory level
+	public let color: AirMapStatus.StatusColor
+	
+	/// A descriptive title
+	public let name: String
+	
+	/// The location of the advisory
+	public let coordinate: Coordinate2D
+	
+	/// The distance from the area that generated the advisory
+	public let distance: Meters
+	
+	/// The advisory location's city
+	public let city: String?
+
+	/// The advisory location's state/province
+	public let state: String?
+
+	/// The advisory location's country
+	public let country: String
+	
+	/// The identifier of the rule that generated the advisory
+	public let ruleId: Int
+
+	/// The identifier of the ruleset from which the rule originated
+	public let ruleSetId: String
+	
+	/// Additional metadata specific to the advisory type
+	public let properties: AdvisoryProperties?
+
+	/// Any requirements necessary to operate within the advisory
+	public let requirements: AirMapStatusRequirements?
+
+	/// The date and time the advisory was last updated
+	public let lastUpdated: Date
+}
+
+public protocol AdvisoryProperties: Mappable {}
+
+// MARK: - Equatable & Hashable
+
+extension AirMapAdvisory: Equatable, Hashable {
+	
+	public var hashValue: Int {
+		return id.hashValue
+	}
+	
+	public static func ==(lhs: AirMapAdvisory, rhs: AirMapAdvisory) -> Bool {
+		return lhs.hashValue == rhs.hashValue
+	}
+}
+
+// MARK: - JSON Serialization
+
 import ObjectMapper
 
-public class AirMapAdvisory: Mappable, Hashable, Equatable {
+extension AirMapAdvisory: ImmutableMappable {
 	
-	public let id: String
-	public let color: AirMapStatus.StatusColor
-	public let name: String
-	public let lastUpdated: Date
-	public let coordinate: Coordinate2D
-	public let distance: Meters
-	public let type: AirMapAirspaceType
-	public let city: String?
-	public let state: String?
-	public let country: String
-	public let ruleId: Int
-	public let ruleSetId: String
-	public let properties: AdvisoryProperties?
-	public let requirements: AirMapStatusRequirements?
-	
-	private static let dateTransform = CustomDateFormatTransform(formatString: Config.AirMapApi.dateFormat)
-	
-	public required init?(map: Map) {
-
+	public init(map: Map) throws {
+		
+	let dateTransform = CustomDateFormatTransform(formatString: Config.AirMapApi.dateFormat)
+		
 		do {
 			id            =  try  map.value("id")
 			color         =  try  map.value("color")
-			lastUpdated   =  try  map.value("last_updated", using: AirMapAdvisory.dateTransform)
+			lastUpdated   = (try? map.value("last_updated", using: dateTransform)) ?? Date()
 			distance      =  try  map.value("distance")
 			type          =  try  map.value("type")
 			city          =  try? map.value("city")
@@ -42,7 +89,11 @@ public class AirMapAdvisory: Mappable, Hashable, Equatable {
 			ruleId        =  try  map.value("rule_id")
 			ruleSetId     =  try  map.value("ruleset_id")
 			requirements  =  try? map.value("requirements")
-			
+
+			let latitude  = try  map.value("latitude") as Double
+			let longitude = try  map.value("longitude") as Double
+			coordinate = Coordinate2D(latitude: latitude, longitude: longitude)
+
 			let airspaceType = try map.value("type") as AirMapAirspaceType
 			name = (try? map.value("name") as String) ?? airspaceType.title
 			
@@ -68,28 +119,12 @@ public class AirMapAdvisory: Mappable, Hashable, Equatable {
 			default:
 				properties = nil
 			}
-			
-			let latitude  = try  map.value("latitude") as Double
-			let longitude = try  map.value("longitude") as Double
-			coordinate = Coordinate2D(latitude: latitude, longitude: longitude)
 		}
 			
 		catch let error {
-			print(error)
-			return nil
+			AirMap.logger.error(error)
+			throw error
 		}
 	}
-	
-	public var hashValue: Int {
-		return id.hashValue
-	}
-	
-	public func mapping(map: Map) {}
-	
-	public static func ==(lhs: AirMapAdvisory, rhs: AirMapAdvisory) -> Bool {
-		return lhs.hashValue == rhs.hashValue
-	}
-
 }
 
-public protocol AdvisoryProperties: Mappable {}
