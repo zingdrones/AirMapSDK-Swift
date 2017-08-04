0c5d5cd3f5c3c2a2e97596263b410c59def9aa0a
Adolfo Martinelli <adolfo@airmap.com>
2017-08-04 16:48:23 -0700

    Cleanup AirMapJurisdiction. Add documentation. Log mapping errors.

--- Source/Core/Models/Rules/AIrMapJurisdiction.swift
+++ Source/Core/Models/Rules/AIrMapJurisdiction.swift
@@ -6,10 +6,22 @@
 //  Copyright © 2017 AirMap, Inc. All rights reserved.
 //
 
-import ObjectMapper
-
-public class AirMapJurisdiction: Mappable {
+/// An entity that has jurisdiction for generating rulesets for a given area
+public struct AirMapJurisdiction {
 	
+	/// The unique identifier for the jurisdiction
+	public let id: Int
+
+	/// The name of the jurisdiction
+	public let name: String
+	
+	/// The jurisdiction's region type
+	public let region: Region
+
+	/// A list rulesets available under this jurisdiction
+	public let ruleSets: [AirMapRuleSet]
+	
+	/// An enumeration of the region types
 	public enum Region: String {
 		case federal
         case federalBackup = "federal backup"
@@ -18,64 +30,38 @@ public class AirMapJurisdiction: Mappable {
 		case county
 		case city
 		case local
-		
-		var order: Int {
-			return [.federal, .federal, .federalStructureBackup, .state, .county, .city, .local].index(of: self)!
-		}
 	}
 	
-	public let id: Int
-	public let name: String
-	public let region: Region
-	public let ruleSets: [AirMapRuleSet]
-	
-	required public init?(map: Map) {
-		
-		do {
-			id       = try map.value("id")
-			name     = try map.value("name")
-			region   = try map.value("region")
-			ruleSets = try map.value("rulesets")
-			
-			ruleSets.forEach { ruleSet in
-				ruleSet.jurisdictionId = id
-				ruleSet.jurisdictionName = name
-				ruleSet.jurisdictionRegion = region
-			}
-		}
-		catch let error {
-			print(error)
-			return nil
-		}
-	}
-	
-	public init(id: Int, name: String, region: Region, ruleSets: [AirMapRuleSet]) {
-		self.id = id
-		self.name = name
-		self.region = region
-		self.ruleSets = ruleSets
-	}
-	
-	public func mapping(map: Map) {}
-	
+	/// A filtered list of all the required rulesets
 	public var requiredRuleSets: [AirMapRuleSet] {
 		return ruleSets.filter { $0.type == .required }
 	}
 	
+	/// A filtered list of all pick-one rulesets
 	public var pickOneRuleSets: [AirMapRuleSet] {
 		return ruleSets.filter { $0.type == .pickOne }
 	}
 	
+	/// The default pick-one ruleset for the jurisdiction
 	public var defaultPickOneRuleSet: AirMapRuleSet? {
 		return pickOneRuleSets.first(where: { $0.isDefault }) ?? pickOneRuleSets.first
 	}
 	
+	/// A filtered list of all optional rulesets
 	public var optionalRuleSets: [AirMapRuleSet] {
 		return ruleSets.filter { $0.type == .optional }
 	}
 	
+	/// A list of AirMap-recommended rulesets for the jurisdiction
 	public var airMapRecommendedRuleSets: [AirMapRuleSet] {
 		return ruleSets.filter { $0.shortName.uppercased() == "AIRMAP" }
+	}
+}
+
+extension AirMapJurisdiction.Region {
+	
+	var order: Int {
+		return [.federal, .federal, .federalStructureBackup, .state, .county, .city, .local].index(of: self)!
 	}
 }
 
@@ -91,5 +77,39 @@ extension AirMapJurisdiction: Hashable, Equatable, Comparable {
 	
 	public var hashValue: Int {
 		return id.hashValue
+	}
+}
+
+// MARK: - JSON Serialization
+
+import ObjectMapper
+
+extension AirMapJurisdiction: ImmutableMappable {
+	
+	public init(map: Map) throws {
+		
+		do {
+			id     = try map.value("id")
+			name   = try map.value("name")
+			region = try map.value("region")
+			
+			guard let ruleSetJSON = map.JSON["rulesets"] as? [[String: Any]] else {
+				throw AirMapError.serialization(.invalidJson)
+			}
+			
+			// Patch the JSON with information about the jurisdiction :/
+			var updatedJSON = [[String: Any]]()
+			for var json in ruleSetJSON {
+				json["jurisdiction"] = ["id": id, "name": name, "region": region.rawValue]
+				updatedJSON.append(json)
+			}
+			
+			let mapper = Mapper<AirMapRuleSet>(context: AirMapRuleSet.Origin.tileService)
+			ruleSets = try mapper.mapArray(JSONArray: updatedJSON)
+		}
+		catch let error {
+			AirMap.logger.error(error)
+			throw error
+		}
 	}
 }
