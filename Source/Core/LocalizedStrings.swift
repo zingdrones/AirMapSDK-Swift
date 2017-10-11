534d802895f0820315e8b18fc7c7688692d7838e
Rocky Demoff <rocky@airmap.com>
2017-10-11 05:58:42 -0700

    Updated rejected_pon_submission status description

--- Source/Core/LocalizedStrings.swift
+++ Source/Core/LocalizedStrings.swift
@@ -37,7 +37,7 @@ public struct LocalizedStrings {
 
 		public static let rejected = NSLocalizedString("AUTHORIZATION_REJECTED", bundle: bundle, value: "Rejected Missing Info", comment: "The request with the authority has been rejected")
 
-		public static let rejectedUponSubmission = NSLocalizedString("AUTHORIZATION_REJECTED_UPON_SUBMISSION", bundle: bundle, value: "Rejected Missing Info", comment: "The request with the authority will be rejected once the flight plan is submitted")
+		public static let rejectedUponSubmission = NSLocalizedString("AUTHORIZATION_REJECTED_UPON_SUBMISSION", bundle: bundle, value: "Rejected Upon Submission", comment: "The request with the authority will be rejected once the flight plan is submitted")
 	}
 	
 	public struct Validation {
