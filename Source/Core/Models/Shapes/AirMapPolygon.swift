b0c980fe1e93546915bb0b1c35cf98d627890c8b
Rocky Demoff <rocky@airmap.com>
2017-10-14 00:42:39 -0700

    Fixed AirMapPolygon

--- Source/Core/Models/Shapes/AirMapPolygon.swift
+++ Source/Core/Models/Shapes/AirMapPolygon.swift
@@ -7,11 +7,10 @@
 //
 
 import Foundation
-
 public class AirMapPolygon: AirMapGeometry {
-
+	
 	public var coordinates: [[Coordinate2D]]!
-		
+	
 	public var type: AirMapFlightGeometryType {
 		return .polygon
 	}
@@ -25,12 +24,21 @@ public class AirMapPolygon: AirMapGeometry {
 		var params = [String: Any]()
 		
 		if (coordinates?.count ?? 0) >= 1 {
+			
+			let coordinates = self.coordinates.map { (coordinates) -> [Coordinate2D] in
+				var newCoords = coordinates.reduce([Coordinate2D]()) { (result, next) -> [Coordinate2D] in
+					var result = result
+					if result.last != next {
+						result.append(next)
+					}
+					return result
+				}
+				return newCoords
+			}
+			
 			params["type"] = "Polygon"
 			params["coordinates"] = coordinates
 				.map { coordinates in
-					var coordinates = coordinates
-					// Connect the last point to the first
-					coordinates.append(coordinates.first!)
 					return coordinates.map { coordinate in
 						[coordinate.longitude, coordinate.latitude]
 					}
@@ -39,5 +47,5 @@ public class AirMapPolygon: AirMapGeometry {
 		
 		return params
 	}
-
 }
+
