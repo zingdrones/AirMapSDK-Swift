c2db5c80fd84026ef33a8f688c86160f93cc2f1f
Michael Odere <michael@airmap.com>
2019-08-05 09:56:59 -0700

    default style url 0.10.0-alpha1 -> 0.10.0 (#93)

--- Source/Core/Constants.swift
+++ Source/Core/Constants.swift
@@ -138,7 +138,7 @@ struct Constants {
 		
 		static var styleUrl: URL {
 			return AirMap.configuration.mapStyle ??
-				URL(string: "https://cdn.airmap.com/static/map-styles/0.10.0-alpha1/")!
+				URL(string: "https://cdn.airmap.com/static/map-styles/0.10.0/")!
 		}
 	}
 }
